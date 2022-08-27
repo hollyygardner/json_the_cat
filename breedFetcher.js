65d9399e603f9fb000236616a468dae90c986db9
Holly Gardner <hollylizgardner@gmail.com>
2022-08-27 13:44:40 -0400

    Description or error via provided callback

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,20 +1,24 @@
 const request = require('request');
-const breed = process.argv[2];
 
-const fetchBreedDescription = function(catBreed) {
-  request(`https://api.thecatapi.com/v1/breeds/search?q=${catBreed}`, (error, response, body) => {
-    console.log('error:', error); // Print the error if one occurred
-    console.log('statusCode:', response && response.statusCode);
-    const data = JSON.parse(body)[0];
-    if (typeof data !== "object") {
-      console.log("Requested breed not found");
+const fetchBreedDescription = function(breedName, callback) {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  request(url, (error, response, body) => {
+
+    if (error) {
+      return callback(error, null);
     }
-    if (typeof data === "object") {
-      console.log(data.description);
+
+    const data = JSON.parse(body);
+    
+    if (Array.isArray(data) && data.length) {
+      return callback(null, data[0].description);
     }
+    if (Array.isArray(data) && !data.length) {
+      return callback(null, null);
+    }
+
   });
 };
 
-fetchBreedDescription(breed);
-
+module.exports = { fetchBreedDescription };
 
